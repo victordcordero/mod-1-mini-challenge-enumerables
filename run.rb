fe64c57b47224262523366366008ead0c055de08
sylwiavargas <sylwia.vargas@gmail.com>
2020-12-08 16:19:00 -0500

    first commit

---
+++ run.rb
@@ -0,0 +1,37 @@
+require 'pry'
+
+spicy_foods = [
+  { name: 'Buffalo Wings', cuisine: 'American', heat_level: 3 },
+  { name: 'Mapo Tofu', cuisine: 'Sichuan', heat_level: 6 },
+  { name: 'Green Curry', cuisine: 'Thai', heat_level: 9 },
+]
+
+def print_spicy_foods(spicy_foods)
+
+end
+
+def get_names(spicy_foods)
+
+end
+
+def spiciest_foods(spicy_foods)
+
+end
+
+def get_spicy_food_by_cuisine(spicy_foods, cuisine)
+
+end
+
+# BONUS Deliverables
+def print_spiciest_foods(spicy_foods)
+
+end
+
+def average_heat_level(spicy_foods)
+
+end
+
+# Use this to test your methods
+# run "ruby run.rb" and try calling the methods from the console
+binding.pry
+"pls"
